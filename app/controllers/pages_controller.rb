f0ec16ee28b33b1a5216a19f98b04736857fc5f7
Caio Andrade <caioertai@gmail.com>
2018-03-19 09:01:51 -0300

    Moved searchs into pages#search / Might move scrapeservice into a SearchController concern in the future

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,16 @@ require 'open-uri'
 # require_relative '../services/scrape'
 
 class PagesController < ApplicationController
-  def home
+  def search
+  end
+
+  def search_by_ean
+    @product = ScrapeService.search_by_ean(params[:ean])
   end
 end
+
+
+
 
 # Service object
 class ScrapeService
@@ -24,6 +31,7 @@ class ScrapeService
   end
 
   def self.search_by_ean(product_ean)
+    @product = Product.new
     get_info("https://consultaremedios.com.br/busca?termo=" + product_ean.to_s)
   end
 
@@ -32,17 +40,19 @@ class ScrapeService
     doc = get_page(product_url)
 
     # Builds the hash
-    info = {}
-    info[:name] = doc.search('.product-header__title').text
-    if doc.search('hr')[0]
-      info[:composition] = doc.search('hr')[0].next_element.next_element.text.split(', ')
-    else
-      info[:composition] = ['Composição indisponível']
+    @product.name = doc.search('.product-header__title').text
+    if doc.search('hr').first
+      doc.search('hr').first.next_element.next_element.text.split(', ').each_with_index do |ingredient, index|
+        product_ingredient = ProductIngredient.new(composition_index: index)
+        product_ingredient.product = @product
+        product_ingredient.ingredient = Ingredient.new(name: ingredient)
+        @product.product_ingredients << product_ingredient
+      end
     end
 
     prices = doc.search('.presentation-offer__price-value strong').map { |price| price.text.delete(',').to_i }
-    info[:price] = Money.new(prices.sum / prices.size, 'BRL')
+    @product.price = prices.sum / prices.size
 
-    info
+    @product
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def monetize(money_integer, currency = 'BRL')
+    Money.new(money_integer, currency).format
+  end
 end
