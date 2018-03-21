8a6612451fc30a35f0c57de09c38b8096088218d
Caio Andrade <caioertai@gmail.com>
2018-03-21 06:40:39 -0300

    Moved root to search#page

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   resources :products
   resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
 
+  root to: 'pages#search'
   get '/search/', to: 'pages#search'
   get '/search_by_ean', to: 'pages#search_by_ean'
   get '/search_by_name', to: 'pages#search_by_name'
